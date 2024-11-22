41edc142957b5b8ed5787393354a9d841a4a1bec
sumantharra123 <sarra039@students.campbellsville.edu>
2024-11-22 14:09:55 +0000

    update all the READ files

---
+++ 01_introduction/a_first.py
@@ -0,0 +1,21 @@
+num1 = 100
+print(num1)
+
+num2 = 200
+print(num2)
+
+num3 = num1 + num2
+
+num3
+"num3"
+
+print(num3)
+print("num3")
+
+print(num3, "num3")
+print("num", num3)
+print("num3 -", num3)
+print("num2 -",num2)
+print("num1 -",num1)
+
+
